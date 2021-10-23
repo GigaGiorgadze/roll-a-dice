14b7e2fe71b027290895d07f82b8ef8c0ae29892
Giga <giga.giorgadze.11@gmail.com>
2021-10-23 22:23:44 +0400

    feat: do all functionals

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ let SidesArray = ['front','back', 'right', 'left', 'top', 'bottom']
 const cube = document.querySelector('.cube')
 const roll = document.querySelector('.roll')
 const oneScore = document.querySelector('.OneScore')
-const TwoScore = document.querySelector('.twoScore')
+const TwoScore = document.querySelector('.TwoScore')
+const pass = document.querySelector('.PassTurn')
 let player1Score = 0
 let player2Score = 0
 let player1Active = true
+let tries = 0
+let spinning = false
 function CheckScore() {
     if (cube.classList.contains('show-front')) {
         if(player1Active){
-            player1Score += 1
-            oneScore.innerHTML = `${player1Score}` 
+            player1Score = 0
+            player1Active = false
+            oneScore.innerHTML = `${player1Score}`
         }else{
-            player2Score += 1
+            player2Score = 0 
+            player1Active = true
+            TwoScore.innerHTML = `${player2Score}`
         }
     }else if(cube.classList.contains('show-back')){
         if(player1Active){
@@ -23,6 +29,7 @@ function CheckScore() {
            
         }else{
             player2Score += 2
+            TwoScore.innerHTML = `${player2Score}`
         }
     }else if(cube.classList.contains('show-right')){
         if(player1Active){
@@ -31,6 +38,7 @@ function CheckScore() {
            
         }else{
             player2Score += 3
+            TwoScore.innerHTML = `${player2Score}`
         }
     }else if(cube.classList.contains('show-left')){
         if(player1Active){
@@ -39,27 +47,34 @@ function CheckScore() {
            
         }else{
             player2Score += 4
+            TwoScore.innerHTML = `${player2Score}`
         }
     }else if(cube.classList.contains('show-top')){
         if(player1Active){
             player1Score += 5
-            oneScore.innerHTML = `${player1Score}` 
-           
+            oneScore.innerHTML = `${player1Score}`   
         }else{
             player2Score += 5
+            TwoScore.innerHTML = `${player2Score}`
         }
     }else{
         if(player1Active){
             player1Score += 6
             oneScore.innerHTML = `${player1Score}` 
-           
         }else{
             player2Score += 6
+            TwoScore.innerHTML = `${player2Score}`
         }
+    }
+    if(player1Score >= 100){
+        alert(`player one won`)
+    }else if(player2Score >= 100){
+        alert(`player two won `)
     }
 }
 
 function RollADice() {
+    spinning = true
     let rolling = setInterval(() => {
         let rando = Math.floor(Math.random() * ((SidesArray.length-1) - 0 + 1) + 0)
         cube.className = 'cube'
@@ -71,13 +86,29 @@ function RollADice() {
             }
         }
         cube.classList.add(`show-${SidesArray[rando]}`)
-        console.log(SidesArray[rando], rando)
         let last = rando    
     }, 100);
     setTimeout(() => {
         clearInterval(rolling)
         CheckScore()
+        spinning = false
     }, 2000);
 }
 
-roll.addEventListener('click', RollADice)
+roll.addEventListener('click', () => {
+    if(!spinning){
+        if(!tries){
+            roll.innerHTML = 'risk again'
+            RollADice()
+            tries++       
+            pass.classList.remove('hidden')
+        }else{
+            RollADice()
+        }
+    }
+})
+pass.addEventListener('click', ()=> {
+    tries = 0
+    player1Active = !player1Active
+    pass.classList.add('hidden')
+})
